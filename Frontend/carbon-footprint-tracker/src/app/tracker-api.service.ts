ad92e2b0a83494f9ae3df94839a074d9394caf63
286193 <bharadwajyamini07@gmail.com>
2024-12-09 15:27:19 +0530

    updated

--- Frontend/carbon-footprint-tracker/src/app/tracker-api.service.ts
+++ Frontend/carbon-footprint-tracker/src/app/tracker-api.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { EnergyData } from './model/EnergyData';
 import { GreenScore } from './model/GreenScore';
+import { Task } from './model/task';
 import { TransportData } from './model/TransportData';
 import { WasteProdData } from './model/WasteProdData';
 
@@ -11,6 +12,9 @@ import { WasteProdData } from './model/WasteProdData';
   providedIn: 'root'
 })
 export class TrackerApiService {
+  moveTaskToCurrent(id: number | undefined) {
+    throw new Error('Method not implemented.');
+  }
 
   baseUrl = 'http://localhost:8080/PlanetWise'
 
@@ -39,7 +43,16 @@ export class TrackerApiService {
   getAllGoals(): Observable<any[]> {
     console.log('ok');
     return this.http.get<any[]>(this.baseUrl+`/goals/allGoals`);
-
   }
+
+  // baseurl + planetwise+user+{username}+addGoal+{goalID};
+
+  // baseurl + planetwise+user+{username}+goals;
+
+  addGoals(goalId:string) {
+    let username = sessionStorage.getItem('username')
+    return this.http.post(this.baseUrl+`/user/${username}/addGoal`,goalId);
+  } 
+
 
 }
